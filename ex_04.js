5a1e771fbd5162fc3f857050c1ef6ebe95767477
LUCAS875237 <lucas.renault@epitech.digital>
2024-10-01 15:54:36 +0200

    Create ex_04.js

---
+++ ex_04.js
@@ -0,0 +1,26 @@
+let a = 2
+let b = 4
+let action = "sub"
+function calculator () {
+if (action === "add"){
+return addition (a, b);
+}
+else if (action === "sub"){
+return substraction(a, b);
+}
+function addition() {
+return a+b
+}
+function substraction (){
+return a-b
+}
+
+}
+
+// DO NOT TOUCH - This line will call calculator and display the result for you
+if (typeof calculator === "function" && typeof a === "number" &&
+		typeof b === "number" && typeof action === "string") {
+	displayResult(calculator, a, b, action);
+} else {
+	console.error("Missing/bad variables or calculator function");
+}
